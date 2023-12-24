5c328b7f2c4a69616df1651799f261487a9b4c5d
witsarut1133@outlook.com <witsarut1133@outlook.com>
2023-12-24 16:24:08 +0700

    add no3

--- no3.cpp
+++ no3.cpp
@@ -1,18 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool isSorted(int arr[],int n,int cur=0){
-  if(n==cur-1){
-    return true;
-  }else{
-    
-    return arr[cur] < arr[cur+1] && isSorted(arr,n,cur+1);
+void getInput(int *arr, int n) {
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
   }
 }
 
-int main(){
-  // int *arr = new int[4];
-  int arr[] = {1,2,4,5};
-  cout<<isSorted(arr,4);
+int inRange(int start, int targetTime, int end) {
+  return targetTime >= start && targetTime <= end ? 1 : 0;
+}
+
+int main() {
+  int n = 5;
+  // cin>>n;
+  int *arr = new int[n]{1, 2, 10, 12, 5};
+  int *brr = new int[n]{10, 10, 12, 18, 10};
+
+  int minTime = arr[0], maxTime = brr[0];
+  for (int i = 0; i < n; i++) {
+    if (arr[i] < minTime) {
+      minTime = arr[i];
+    }
+    if (brr[i] > maxTime) {
+      maxTime = brr[i];
+    }
+  }
+
+  int maxPresent = 0;
+  int idx = -1;
+  for (int i = minTime; i <= maxTime; i++) {
+    int curPresent = 0;
+    for (int j = 0; j < n; j++) {
+      curPresent += inRange(arr[j], i, brr[j]);
+    }
+    if (curPresent > maxPresent) {
+      maxPresent = curPresent;
+      idx = i;
+    }
+  }
+  cout << idx << " " << maxPresent << endl;
   return 0;
 }
