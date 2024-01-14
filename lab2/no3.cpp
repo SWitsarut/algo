22e29ca9472d3ef5e104e51400924c859737fbd8
s <s@s.s>
2024-01-15 01:47:19 +0700

    fix no3 and update git ignore

--- lab2/no3.cpp
+++ lab2/no3.cpp
@@ -1,57 +1,77 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   int n = 0;
   cin >> n;
-  int *uni = new int[n * 2];
+
+  int *uni = new int[n * 2]; 
   int unisize = 0;
+
   int *ins = new int[n];
   int inssize = 0;
 
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     int temp = 0;
     cin >> temp;
+
     bool found = false;
-    for (int j = 0; j < unisize; j++) {
-      if (temp == uni[j]) {
+    for (int j = 0; j < unisize; j++)
+    {
+      if (temp == uni[j])
+      {
         found = true;
         break;
       }
     }
-    if (!found) {
+
+    if (!found)
+    {
       uni[unisize] = temp;
       unisize++;
     }
   }
 
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     int temp;
     cin >> temp;
+
     bool found = false;
-    for (int j = 0; j < unisize; j++) {
-      if (temp == uni[j]) {
+    for (int j = 0; j < unisize; j++)
+    {
+      if (temp == uni[j])
+      {
         ins[inssize] = temp;
         inssize++;
         found = true;
         break;
       }
     }
-    if (!found) {
+
+    if (!found)
+    {
       uni[unisize] = temp;
       unisize++;
     }
   }
-  for (int i = 0; i < inssize - 1; i++) {
+
+  for (int i = 0; i < inssize; i++)
+  {
     cout << ins[i] << " ";
   }
   cout << endl;
 
-  for (int i = 0; i < unisize; i++) {
+  for (int i = 0; i < unisize; i++)
+  {
     cout << uni[i] << " ";
   }
   cout << endl;
-  delete  [] uni;
-  delete [] ins;
+
+  delete[] uni;
+  delete[] ins;
+
   return 0;
 }
